24a67ba1d536aaf5aff2029f37a6b70e10e577c0
Fateeva <anastasiafateeva2406@gmail.com>
2024-03-05 14:03:47 +0300

    init app

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+    return (
+        <header className={'header'}>
+            <img
+                src={'https://images.vexels.com/media/users/3/272593/isolated/preview/2c91055c5e95c3e07ead4d39c0a2b2e8-minimalist-flower-icon.png'}
+                alt={'header'}/>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Profile/Profile.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className={'content'}>
+            <img
+                src={'https://camo.githubusercontent.com/207d1b81e905f24852f7d86878220c80d116963df0c15d60afd88cbd5a4a6720/68747470733a2f2f6e797473656e73652e636f6d2f50494354555245532f436c6f756473322e706e67'}
+                alt={'header'}/>
+            <div>
+                avatar + description
+            </div>
+            <div>My posts</div>
+            <div>New post</div>
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile
